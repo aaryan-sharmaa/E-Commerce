7e57c957f3c5c33c93300c6a1cc397b71b5c7375
Aryan Sharma <aryansharma24022004@gmail.com>
2024-05-11 23:19:49 +0530

    Fetch Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Modules/Home";
 import Footer from "./Components/Footer";
+import { Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Router>
+        <Route path="/" element={<Home />} />
+      </Router>
       <Footer />
     </>
   );
