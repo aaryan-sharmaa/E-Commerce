0f09400d6f9822084e7a322f7dfc502160e9e35e
Aryan Sharma <aryansharma24022004@gmail.com>
2024-05-11 22:36:25 +0530

    Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Home from "./Modules/Home";
 
 function App() {
   return (
--- src/Modules/Home/index.jsx
+++ src/Modules/Home/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Hero from "../../Components/Hero";
+import Product from "../../Components/Products";
+import StatCard from "../../Components/Statstics";
 
-function Home () {
-    return (
-        <Hero />
-    )
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <div className="flex flex-col text-center w-full mt-20"></div>
+      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1 ">PRODUCTS</h2>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">MOST POPULAR PRODUCTS</h1>
+      <Product />
+      <StatCard />
+
+    </>
+  );
 }
 
 export default Home;
